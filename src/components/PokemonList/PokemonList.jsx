7cfa0475c0acf886d532884e3ddf2a74089fec59
rishabh verma <2021aspire28@gmail.com>
2024-04-11 22:48:45 +0530

    added css and next and prev functionalities

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -7,13 +7,22 @@ function PokemonList(){
     const [pokemonList,setPokemonList] = useState([]);
     const [isLoading,setIsLoading] = useState(true);
 
-    const pokedex_url = 'https://pokeapi.co/api/v2/pokemon';
+    const [pokedex_url,setPokedex_url] = useState('https://pokeapi.co/api/v2/pokemon');
+
+    const [nextUrl,setNextUrl] = useState('');
+    const [prevUrl,setPrevUrl] = useState('');
 
     async function downloadPokemons(){
-
-        const response = await axios.get('pokedex_url');//this downloads 20 pokemons
+        setIsLoading(true);
+        const response = await axios.get(pokedex_url);//this downloads 20 pokemons
         
         const pokemonResults = response.data.results;//we get the array of pokemons from results
+        
+        console.log(response.data);
+        setNextUrl(response.data.next);
+        setPrevUrl(response.data.previous);
+
+
         //iterating over the array of pokemons ans using their url to create an array of propmises that will download those 20 
         const pokemonResultPromise = pokemonResults.map((pokemon)=>axios.get(pokemon.url));
         
@@ -38,14 +47,20 @@ function PokemonList(){
     //callback and dependency array expect krta hai
     useEffect(()=>{
         downloadPokemons();
-    },[]);
+    },[pokedex_url]);
     
 
     return (
     <div className="pokemon-list-wrapper">
-         POKEMON LIST
-        {(isLoading) ? 'Loading...' :
+         <div className="pokemon-wrapper">
+         {(isLoading) ? 'Loading...' :
         pokemonList.map((p)=> <Pokemon name={p.name} image={p.image} key={p.id}/>) }
+         </div>
+        
+        <div className="controls">
+            <button disabled={prevUrl == null} onClick={()=> setPokedex_url(prevUrl)}>Prev</button>
+            <button disabled={nextUrl == null} onClick={()=> setPokedex_url(nextUrl)}>Next</button>
+        </div>
     </div>
     
     )
